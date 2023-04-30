2e1d811c76da4548df95a6cb6aeeba301ec4414a
FairySunny <fairysunny0214@gmail.com>
2023-04-30 18:09:48 +0800

    shift characters according to glyph_sizes.bin

--- scripts/generate-font-bitmap.py
+++ scripts/generate-font-bitmap.py
@@ -1,8 +1,12 @@
 import sys
+import json
 import cv2 as cv
 
 if len(sys.argv) != 3:
     print(f'Usage: python {sys.argv[0]} <font directory> <output file>')
+
+with open(f'{sys.argv[1]}/glyph_sizes.bin', 'rb') as f:
+    sizes = f.read()
 
 with open(sys.argv[2], 'wb') as f:
     for idx in range(256):
@@ -13,8 +17,12 @@ with open(sys.argv[2], 'wb') as f:
         else:
             for char_row in range(16):
                 for char_col in range(16):
+                    char_idx = idx << 8 | char_row * 16 + char_col
+                    left = sizes[char_idx] >> 4
+                    right = sizes[char_idx] & 0xf
+                    shift = 7 - (left + right) // 2
                     for pixel_row in range(16):
                         b = 0
-                        for pixel_col in range(16):
-                            b |= int(img[char_row * 16 + pixel_row, char_col * 16 + pixel_col, 3] >= 128) << pixel_col
+                        for pixel_col in range(left, right + 1):
+                            b |= int(img[char_row * 16 + pixel_row, char_col * 16 + pixel_col, 3] >= 128) << pixel_col + shift
                         f.write(bytes([b & 0xff, b >> 8]))
